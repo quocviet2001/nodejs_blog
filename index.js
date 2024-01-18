cc14e5ae67a5c082b10083c822842133cfac9ec0
quocviet2001 <nguyentun16012001@gmail.com>
2024-01-18 09:31:24 +0700

    bootstrap

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const post = 3000
-
-app.get('/', (req, res) => res.send('Hello World'))
-
-app.listen(post, () => console.log(`http://localhost:${post}`))
